e5280f5e88d0d7fab4ec3567141b92b8721899fa
Bryan Matthews <mattheyan@gmail.com>
2012-05-04 22:58:14 -0400

    Custom Validation

--- TodoApp/Models/ListItem.cs
+++ TodoApp/Models/ListItem.cs
@@ -28,5 +28,21 @@ namespace TodoApp.Models
 
 		[AllowedValues("Priority.All")]
 		public Priority Priority { get; set; }
+
+		/// <summary>
+		/// Ensures that the due date is on or after the day on which the item was created.
+		/// </summary>
+		static Error ValidateFutureDueDate = new Error<ListItem>(
+			"The Due Date must be today or some time in the future",
+			item => item.DateCreated != null && item.DueDate != null && item.DueDate.Value < item.DateCreated.Value.Date);
+
+		/// <summary>
+		/// Ensure that items in the same list have unique text descriptions.
+		/// </summary>
+		static Error ValidateUniqueText = new Error<ListItem>(
+			"This appears to be a duplicate!",
+			item => item.List != null && item.List.Items.Any(i => i != item && i.Description == item.Description),
+			"Description")			// Only attach the error to the Description property on the root instance
+			.OnInitExisting();		// Ensure validation runs on existing instances since this rule is not enforced
 	}
 }
