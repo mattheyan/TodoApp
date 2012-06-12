48fa190831826650ea8e8af6689e635071c2bd97
Bryan Matthews <mattheyan@gmail.com>
2012-06-11 22:03:25 -0400

    enable database initializer

--- TodoApp/App_Start/DontDropDbJustCreateTablesIfModelChangedStart.cs
+++ TodoApp/App_Start/DontDropDbJustCreateTablesIfModelChangedStart.cs
@@ -8,7 +8,7 @@ namespace TodoApp.App_Start {
         public static void Start() {
             // Uncomment this line and replace CONTEXT_NAME with the name of your DbContext if you are 
             // using your DbContext to create and manage your database
-            // Database.SetInitializer(new DontDropDbJustCreateTablesIfModelChanged<CONTEXT_NAME>());
+            Database.SetInitializer(new DontDropDbJustCreateTablesIfModelChanged<TodoApp.Models.TodoContext>());
         }
     }
 }
